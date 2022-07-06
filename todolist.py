fc091ba68b3a4bc960995f1d7fd32a93a1334e71
Ishita Garg <92790319+Ishita056@users.noreply.github.com>
2022-07-06 20:04:29 +0530

    updated todolist

--- todolist.py
+++ todolist.py
@@ -2,7 +2,6 @@ from tkinter import *
 import random
 from tkinter import messagebox
 
-# defining functions
 def updatetask():
     clearlistbox()
     for utask in tasks:
@@ -10,10 +9,8 @@ def updatetask():
     numtask = len(tasks)
     lbldsp_count['text'] = numtask
 
-
 def clearlistbox():
     lbltask.delete(0, "end")
-
 
 def addtask():
     lbldisplay["text"] = ""
@@ -25,17 +22,8 @@ def addtask():
         lbldisplay["text"] = "please enter the text"
     txtinput.delete(0, 'end')
 
-
-def deleteone():
-    delt = lbltask.get("active")
-    if delt in tasks:
-        tasks.remove(delt)
-    updatetask()
-
-
 def deleteall():
-    conf = messagebox.askquestion(
-        'Delete all??', 'are you sure to delete all task?')
+    conf = messagebox.askquestion('Delete all??', 'are you sure to delete all task?')
     # print(conf)
     if conf.upper() == "YES":
         global tasks
@@ -44,51 +32,92 @@ def deleteall():
     else:
         pass
 
+def deleteone():
+    delt = lbltask.get("active")
+    if delt in tasks:
+        tasks.remove(delt)
+    updatetask()
 
-def numbertsk():
-    numtask = len(tasks)
-    lbldisplay["text"] = numtask
+def sortasc():
+    tasks.sort()
+    updatetask()
 
-# making window
+def sortdsc():
+    tasks.sort(reverse=True)
+    updatetask()
+
+def randomtsk():
+    randtask = random.choice(tasks)
+    lbldisplay["text"] = randtask
+
+def saveact():
+    savecon = messagebox.askquestion('Save confirmation', 'Save Your Progress?')
+    if savecon.upper() == "YES":
+        with open("SaveFile.txt", "w") as filehandle:
+            for listitem in tasks:
+                filehandle.write('%s\n' % listitem)
+    else:
+        pass
+
+def exitapp():
+    confex = messagebox.askquestion(
+        'Quit confirmation', 'Are you sue you want to quit?')
+    if confex.upper() == "YES":
+        root.destroy()
+    else:
+        pass
+
 root = Tk()
 root.configure(bg="white")
 root.title("To-Do List")
-root.geometry("250x300")
+root.geometry("255x250")
+root.wm_iconbitmap("list_icon.ico")
 tasks = []
 
+#title
 lbltitle = Label(root, text="Todo List", bg="white")
 lbltitle.grid(row=0, column=0)
 
+#display window
 lbldisplay = Label(root, text="", bg="white")
 lbldisplay.grid(row=0, column=1)
 
+# counting tasks
 lbldsp_count = Label(root, text="", bg="white")
 lbldsp_count.grid(row=0, column=3)
 
+# enter text
 txtinput =Entry(root, width=15)
 txtinput.grid(row=1, column=1)
 
 # adding buttons
-txtadd_button = Button(
-    root, text="Add todo", bg="white", fg="red", width=15, command=addtask)
+txtadd_button = Button(root, text="Add todo", bg="white", fg="red", width=15, command=addtask)
 txtadd_button.grid(row=1, column=0)
 
-delonebutton = Button(
-    root, text="Done Task", bg="white", width=15, command=deleteone)
+delonebutton = Button(root, text="Done Task", bg="white", width=15, command=deleteone)
 delonebutton.grid(row=2, column=0)
 
-delallbutton = Button(
-    root, text="Delete all", bg="white", width=15, command=deleteall)
+delallbutton = Button(root, text="Delete all", bg="white", width=15, command=deleteall)
 delallbutton.grid(row=3, column=0)
 
-numbertsk = Button(
-    root, text="Number of Task", bg="white", width=15, command=numbertsk)
-numbertsk.grid(row=7, column=0)
+sortasc = Button(root, text="sort (ASC)", bg="White", width=15, command=sortasc)
+sortasc.grid(row=4, column=0)
+
+sortdsc = Button(root, text="sort (DSC)", bg="White", width=15, command=sortdsc)
+sortdsc.grid(row=5, column=0)
+
+randombutton = Button(root, text="random task", bg="White", width=15, command=randomtsk)
+randombutton.grid(row=6, column=0)
+
+savebutton = Button(root, text="save TodoList", bg="white", width=15, command=saveact)
+savebutton.grid(row=7, column=0)
+
+exitbutton = Button(root, text="exit app", bg="white", width=15, command=exitapp)
+exitbutton.grid(row=8, column=0)
 
 # display tasks
 lbltask = Listbox(root)
-lbltask.grid(row=2, column=1, rowspan=7, columnspan=4)
+lbltask.grid(row=2, column=1, rowspan=12, columnspan=2)
 
-
-
-root.mainloop() 
+# main loop
+root.mainloop()
