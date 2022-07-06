8f2e1296e07355afbbf52a0b4126e0dc7e29dcbc
Ishita Garg <92790319+Ishita056@users.noreply.github.com>
2022-07-06 19:27:26 +0530

    Updated buttons

--- buttons.py
+++ buttons.py
@@ -1,21 +1,31 @@
-# import everything from tkinter module
 from tkinter import *
 import tkinter as tk
 
-# create a tkinter window
 root = Tk()	
 
-# Create label
-head = Label(root, text = """Python Mini Project
-     Welcome to Multi-Functional Notepad. 
-         Please choose your option.""")
-head.config(font =("Courier", 14))
+# Open window having dimension 100x100
+root.geometry('400x250')
 
+# add bg
+bg = PhotoImage(file = "bg.png")
+canvas1 = Canvas(root, width=400, height=250)
+canvas1.pack(fill = "both", expand = True)
+canvas1.create_image(0, 0, image = bg, anchor = "nw")
 
-head.pack()
+# add icons
+icon1 = PhotoImage(file = "notepad_image.png", height=30, width=30)
+canvas1.create_image(130, 93, image = icon1, anchor = "nw")
 
-# Open window 
-root.geometry('500x400')
+icon2 = PhotoImage(file = "list_image.png", height=30, width=30)
+canvas1.create_image(130, 143, image = icon2, anchor = "nw")
+
+icon3 = PhotoImage(file = "alarm_image.png", height=30, width=30)
+canvas1.create_image(130, 193, image = icon3, anchor = "nw")
+
+# add heading
+canvas1.create_text(60,10, text = """                Python Mini Project
+Welcome to Multi-Functional Notepad. 
+           Please choose your option.""", anchor = "nw", fill = "black", font = ("Cooper Black", 11))
 
 def notepad():
     root.destroy()
@@ -25,18 +35,18 @@ def todo():
     root.destroy()
     import todolist
 
-# Create a Button
-btn1 = Button(root, text = 'Notepad', bd = '5',
-						command = notepad)
+def alarm():
+    root.destroy()
+    import Alarm
 
-# Set the position of button on the top of window.
-btn1.place(x=210,y=100)
-    
 # Create a Button
-btn2 = Button(root, text = 'To-Do List', bd = '5',
-						command = todo )
+btn = Button(root, text = 'Notepad', bd = '7', bg = "#f27e90", command = notepad)
+btn.place(x=170,y=90)
 
-# Set the position of button on the top of window.
-btn2.place(x=210,y=200)
+btn2 = Button(root, text = 'To-Do List', bd = '7', bg = "#f27e90", command = todo )
+btn2.place(x=170,y=140)
+
+btn3 = Button(root, text = 'Alarm', bd = '7', bg = "#f27e90", command = alarm )
+btn3.place(x=170,y=190)
 
 root.mainloop()
